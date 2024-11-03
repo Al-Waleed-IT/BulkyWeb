8cba97cc7d68f18246eae2cabdc628911cf4975b
Tohidul Islam <raptor.dnj@gmail.com>
2024-11-03 06:06:52 +0600

    Update

--- BulkyWeb/obj/Release/net8.0/BulkyWeb.AssemblyInfo.cs
+++ BulkyWeb/obj/Release/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+934d146c65235fff6b113573310a50bf7dc9d0da")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
