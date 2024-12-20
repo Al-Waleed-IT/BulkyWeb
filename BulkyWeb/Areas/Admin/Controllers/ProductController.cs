1ca0b26012b74c3400eb3f2059a83e01ee37f75b
Tohidul Islam <raptor.dnj@gmail.com>
2024-12-21 01:12:57 +0600

    Homepage Added

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using Bulky.Models.ViewModels;
 using BulkyWeb.Data.Repository;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWeb.Areas.Admin
 {
@@ -73,8 +74,19 @@ namespace BulkyWeb.Areas.Admin
                     
                     obj.Product.ImageUrl = @"/images/product/" + fileName;
                 }
-                _unitOfWork.Product.Add(obj.Product);
+
+                if (obj.Product.Id == 0)
+                {
+                    _unitOfWork.Product.Add(obj.Product);
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(obj.Product);
+                }
+                
+
                 _unitOfWork.Product.Save();
+                
                 TempData["Success"] = $"{_componentTitle} created successfully";
                 return RedirectToAction("Index");
             }
@@ -169,5 +181,84 @@ namespace BulkyWeb.Areas.Admin
 
             return NotFound();
         }
+
+        #region API CALLS
+
+[HttpGet]
+[HttpGet]
+public IActionResult GetProducts()
+{
+    // Get DataTable request parameters
+    var draw = HttpContext.Request.Query["draw"].FirstOrDefault();
+    var start = HttpContext.Request.Query["start"].FirstOrDefault();
+    var length = HttpContext.Request.Query["length"].FirstOrDefault();
+    var sortColumnIndex = HttpContext.Request.Query["order[0][column]"].FirstOrDefault();
+    var sortDirection = HttpContext.Request.Query["order[0][dir]"].FirstOrDefault();
+    var searchValue = HttpContext.Request.Query["search[value]"].FirstOrDefault();
+
+    // Convert parameters to appropriate data types
+    int pageSize = string.IsNullOrEmpty(length) ? 0 : Convert.ToInt32(length);
+    int skip = string.IsNullOrEmpty(start) ? 0 : Convert.ToInt32(start);
+
+    // Get the data source as an IQueryable
+    var productQuery = _unitOfWork.Product.GetAll(); // Ensure GetAll() returns IQueryable<Product>
+
+    // Apply filtering
+    if (!string.IsNullOrEmpty(searchValue))
+    {
+        productQuery = productQuery.Where(p =>
+            p.Title.Contains(searchValue) ||  // Replace 'Title' with the actual property
+            p.Author.Contains(searchValue)); // Replace 'Author' with the actual property
+    }
+
+    // Apply sorting
+    switch (sortColumnIndex)
+    {
+        case "0": // ID column
+            productQuery = sortDirection == "asc"
+                ? productQuery.OrderBy(p => p.Id)
+                : productQuery.OrderByDescending(p => p.Id);
+            break;
+        case "1": // Title column
+            productQuery = sortDirection == "asc"
+                ? productQuery.OrderBy(p => p.Title)
+                : productQuery.OrderByDescending(p => p.Title);
+            break;
+        case "2": // ISBN column
+            productQuery = sortDirection == "asc"
+                ? productQuery.OrderBy(p => p.ISBN)
+                : productQuery.OrderByDescending(p => p.ISBN);
+            break;
+        case "3": // Author column
+            productQuery = sortDirection == "asc"
+                ? productQuery.OrderBy(p => p.Author)
+                : productQuery.OrderByDescending(p => p.Author);
+            break;
+        case "4": // Price column
+            productQuery = sortDirection == "asc"
+                ? productQuery.OrderBy(p => p.Price)
+                : productQuery.OrderByDescending(p => p.Price);
+            break;
+    }
+
+    // Get total record count before pagination
+    var totalRecords = productQuery.Count();
+
+    // Apply pagination
+    var data = productQuery.Skip(skip).Take(pageSize).ToList();
+
+    // Return data in the format required by DataTables
+    return Json(new
+    {
+        draw = draw,
+        recordsTotal = totalRecords,
+        recordsFiltered = totalRecords, // Adjust if additional filtering is applied
+        data = data
+    });
+}
+
+
+
+        #endregion
     }
 }
--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using System.Diagnostics;
+using Bulky.Models;
+using BulkyWeb.Data.Repository;
 using Microsoft.AspNetCore.Mvc;
 using BulkyWeb.Models;
 
@@ -8,16 +10,26 @@ namespace BulkyWeb.Areas.Customer.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
     {
         _logger = logger;
+        _unitOfWork = unitOfWork;
     }
 
     public IActionResult Index()
     {
-        return View();
+        IEnumerable<Product> productList = _unitOfWork.Product.GetAll();
+        return View(productList);
     }
+    
+    public IActionResult Details(int id)
+    {
+        Product product = _unitOfWork.Product.Get(u => u.Id == id);
+        return View(product);
+    }
+
 
     public IActionResult Privacy()
     {
